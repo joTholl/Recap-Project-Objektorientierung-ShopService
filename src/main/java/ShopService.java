b61d26eaf4202212d3cc1fb02f2f046a4cdf8a0e
Jonas Tholl <jonas@tholls.de>
2025-12-19 10:26:30 +0100

    Added getOrdersByOrderStatus and test

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class ShopService {
@@ -9,16 +10,24 @@ public class ShopService {
     public Order addOrder(List<String> productIds) {
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Product productToOrder = productRepo.getProductById(productId);
-            if (productToOrder == null) {
+            Optional<Product> productToOrder = productRepo.getProductById(productId);
+            if (productToOrder.isEmpty()) {
                 System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
                 return null;
             }
-            products.add(productToOrder);
+            products.add(productToOrder.get());
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products,OrderStatus.PROCESSING);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
+    }
+
+    public List<Order> getOrdersByOrderStatus(OrderStatus orderStatus) {
+        List<Order> orders = new ArrayList<>(orderRepo.getOrders());
+        return orders.stream()
+                .filter(order -> order.orderStatus() == orderStatus)
+                .toList();
+
     }
 }
